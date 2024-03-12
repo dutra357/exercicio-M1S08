1e3ee9c30e4ec733e1f19c77d6fbb9f30e01a15a
David Dutra <dadutra@hotmail.com>
2024-03-12 17:51:23 -0300

    Projeto Spring criado - ex.01

---
+++ src/main/java/br/fullstack/exercicioM1S08/ExercicioM1S08Application.java
@@ -0,0 +1,13 @@
+package br.fullstack.exercicioM1S08;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExercicioM1S08Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExercicioM1S08Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/fullstack/exercicioM1S08/ExercicioM1S08ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.fullstack.exercicioM1S08;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExercicioM1S08ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
